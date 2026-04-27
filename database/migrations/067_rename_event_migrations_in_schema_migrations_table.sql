b2ef2e1728025e54953646d74ec271d81d84ba90
Dev Team <dev@daems.fi>
2026-04-27 22:16:50 +0300

    Migration(067-fix): schema_migrations column is filename, not migration

--- database/migrations/067_rename_event_migrations_in_schema_migrations_table.sql
+++ database/migrations/067_rename_event_migrations_in_schema_migrations_table.sql
@@ -5,23 +5,23 @@
 SET @t := (SELECT COUNT(*) FROM information_schema.tables
            WHERE table_schema = DATABASE() AND table_name = 'schema_migrations');
 
-SET @sql := IF(@t > 0, 'UPDATE schema_migrations SET migration = ''event_001_create_events_table.sql'' WHERE migration = ''001_create_events_table.sql''', 'DO 0');
+SET @sql := IF(@t > 0, 'UPDATE schema_migrations SET filename = ''event_001_create_events_table.sql'' WHERE filename = ''001_create_events_table.sql''', 'DO 0');
 PREPARE stmt FROM @sql; EXECUTE stmt; DEALLOCATE PREPARE stmt;
 
-SET @sql := IF(@t > 0, 'UPDATE schema_migrations SET migration = ''event_002_create_event_registrations.sql'' WHERE migration = ''012_create_event_registrations.sql''', 'DO 0');
+SET @sql := IF(@t > 0, 'UPDATE schema_migrations SET filename = ''event_002_create_event_registrations.sql'' WHERE filename = ''012_create_event_registrations.sql''', 'DO 0');
 PREPARE stmt FROM @sql; EXECUTE stmt; DEALLOCATE PREPARE stmt;
 
-SET @sql := IF(@t > 0, 'UPDATE schema_migrations SET migration = ''event_003_add_tenant_id_to_events.sql'' WHERE migration = ''025_add_tenant_id_to_events.sql''', 'DO 0');
+SET @sql := IF(@t > 0, 'UPDATE schema_migrations SET filename = ''event_003_add_tenant_id_to_events.sql'' WHERE filename = ''025_add_tenant_id_to_events.sql''', 'DO 0');
 PREPARE stmt FROM @sql; EXECUTE stmt; DEALLOCATE PREPARE stmt;
 
-SET @sql := IF(@t > 0, 'UPDATE schema_migrations SET migration = ''event_004_add_tenant_id_to_event_registrations.sql'' WHERE migration = ''032_add_tenant_id_to_event_registrations.sql''', 'DO 0');
+SET @sql := IF(@t > 0, 'UPDATE schema_migrations SET filename = ''event_004_add_tenant_id_to_event_registrations.sql'' WHERE filename = ''032_add_tenant_id_to_event_registrations.sql''', 'DO 0');
 PREPARE stmt FROM @sql; EXECUTE stmt; DEALLOCATE PREPARE stmt;
 
-SET @sql := IF(@t > 0, 'UPDATE schema_migrations SET migration = ''event_005_add_status_to_events.sql'' WHERE migration = ''043_add_status_to_events.sql''', 'DO 0');
+SET @sql := IF(@t > 0, 'UPDATE schema_migrations SET filename = ''event_005_add_status_to_events.sql'' WHERE filename = ''043_add_status_to_events.sql''', 'DO 0');
 PREPARE stmt FROM @sql; EXECUTE stmt; DEALLOCATE PREPARE stmt;
 
-SET @sql := IF(@t > 0, 'UPDATE schema_migrations SET migration = ''event_006_create_events_i18n.sql'' WHERE migration = ''051_create_events_i18n.sql''', 'DO 0');
+SET @sql := IF(@t > 0, 'UPDATE schema_migrations SET filename = ''event_006_create_events_i18n.sql'' WHERE filename = ''051_create_events_i18n.sql''', 'DO 0');
 PREPARE stmt FROM @sql; EXECUTE stmt; DEALLOCATE PREPARE stmt;
 
-SET @sql := IF(@t > 0, 'UPDATE schema_migrations SET migration = ''event_007_create_event_proposals.sql'' WHERE migration = ''056_create_event_proposals.sql''', 'DO 0');
+SET @sql := IF(@t > 0, 'UPDATE schema_migrations SET filename = ''event_007_create_event_proposals.sql'' WHERE filename = ''056_create_event_proposals.sql''', 'DO 0');
 PREPARE stmt FROM @sql; EXECUTE stmt; DEALLOCATE PREPARE stmt;
